7d5939dc742dec5df05865e69e42825a22fe21ac
Oren Nachman <git@nachmore.com>
2011-08-04 09:10:10 +0000

    Added missing OppositeBoolConverter.cs (a binding converter which returns !bool)

---
+++ Toastify/OppositeBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Toastify
+{
+    public class OppositeBoolConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // Note: don't check validity etc, since we'll need to throw an exception anyway, may as well let
+            //       that exception be here and not waste time :)
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
