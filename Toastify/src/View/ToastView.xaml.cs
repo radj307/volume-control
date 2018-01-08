radj307/volume-control

commit 2112ad497b5285d1a6d80cf2aca05360de134274

Maintenance: Remove self-assignment

Toastify/src/View/ToastView.xaml.cs
@@ -890,8 +890,8 @@ namespace Toastify.View
                     break;
 
                 case nameof(this.Settings.ToastTitlesOrder):
-                    this.toastViewModel.TrackName = this.toastViewModel.TrackName;
-                    this.toastViewModel.ArtistName = this.toastViewModel.ArtistName;
+                    this.toastViewModel.SetTrackName(this.toastViewModel.TrackName);
+                    this.toastViewModel.SetArtistName(this.toastViewModel.ArtistName =;
                     break;
 
                 case nameof(this.Settings.ToastColorTop):

Toastify/src/ViewModel/ToastViewModel.cs
@@ -61,34 +61,14 @@ namespace Toastify.ViewModel
 
         public string TrackName
         {
-            get
-            {
-                return this._trackName;
-            }
-            set
-            {
-                this._trackName = value;
-                if (this.Settings.ToastTitlesOrder == ToastTitlesOrder.TrackByArtist)
-                    this.Title1 = $"{(string.IsNullOrWhiteSpace(value) ? "???" : value)}";
-                else
-                    this.Title2 = string.IsNullOrWhiteSpace(value) ? string.Empty : $"\x201C{value}\x201D";
-            }
+            get { return this._trackName; }
+            set { this.SetTrackName(value); }
         }
 
         public string ArtistName
         {
-            get
-            {
-                return this._artistName;
-            }
-            set
-            {
-                this._artistName = value;
-                if (this.Settings.ToastTitlesOrder == ToastTitlesOrder.ArtistOfTrack)
-                    this.Title1 = $"{(string.IsNullOrWhiteSpace(value) ? "???" : value)}:";
-                else
-                    this.Title2 = string.IsNullOrWhiteSpace(value) ? string.Empty : $"by {value}";
-            }
+            get { return this._artistName; }
+            set { this.SetArtistName(value); }
         }
 
         public double SongProgressBarWidth
@@ -102,6 +82,24 @@ namespace Toastify.ViewModel
         public ToastViewModel()
         {
             this.Settings = Settings.Current;
+        }
+
+        public void SetTrackName(string trackName)
+        {
+            this._trackName = trackName;
+            if (this.Settings.ToastTitlesOrder == ToastTitlesOrder.TrackByArtist)
+                this.Title1 = $"{(string.IsNullOrWhiteSpace(trackName) ? "???" : trackName)}";
+            else
+                this.Title2 = string.IsNullOrWhiteSpace(trackName) ? string.Empty : $"\x201C{trackName}\x201D";
+        }
+
+        public void SetArtistName(string artistName)
+        {
+            this._artistName = artistName;
+            if (this.Settings.ToastTitlesOrder == ToastTitlesOrder.ArtistOfTrack)
+                this.Title1 = $"{(string.IsNullOrWhiteSpace(artistName) ? "???" : artistName)}:";
+            else
+                this.Title2 = string.IsNullOrWhiteSpace(artistName) ? string.Empty : $"by {artistName}";
         }
 
         private void Settings_PropertyChanged(object sender, PropertyChangedEventArgs e)

commit 90da2297e9bde82588c3cbe90a188280d4077f5a

Fix typo introduced in 2112ad4

Toastify/src/View/ToastView.xaml.cs
@@ -889,7 +889,7 @@ namespace Toastify.View
 
                 case nameof(this.Settings.ToastTitlesOrder):
                     this.toastViewModel.SetTrackName(this.toastViewModel.TrackName);
-                    this.toastViewModel.SetArtistName(this.toastViewModel.ArtistName =;
+                    this.toastViewModel.SetArtistName(this.toastViewModel.ArtistName);
                     break;
 
                 case nameof(this.Settings.ToastColorTop):