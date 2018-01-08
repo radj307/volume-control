79a29e532df51b5d62b9c284e5f2c3774e85b4c9
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2018-01-08 19:13:07 +0100

    Maintenance: Assignments should not be made from within sub-expressions

--- Toastify/src/Win32API.cs
+++ Toastify/src/Win32API.cs
@@ -362,7 +362,10 @@ namespace Toastify
             //System.Diagnostics.Debug.WriteLine("shift: " + shiftKey.State + " alt: " + altKey.State + " ctrl: " + ctrlKey.State);
 
             while (maxSleep > 0 && (shiftKey.State != 0 || altKey.State != 0 || ctrlKey.State != 0))
-                Thread.Sleep(maxSleep -= 50);
+            {
+                Thread.Sleep(maxSleep);
+                maxSleep -= 50;
+            }
 
             //System.Diagnostics.Debug.WriteLine("maxSleep: " + maxSleep);
 
