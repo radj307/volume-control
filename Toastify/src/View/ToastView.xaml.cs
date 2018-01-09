radj307/volume-control

commit c86979fb29e241ba452c06bedddb37e3dc708ad8

Maintenance: Add default label in switch statements

Toastify/src/View/ToastView.xaml.cs
@@ -676,6 +676,9 @@ namespace Toastify.View
                     this.toastIconURI = "pack://application:,,,/Toastify;component/Resources/thumbs_down.png";
                     this.UpdateToastText(this.currentSong, "Thumbs Down!");
                     break;
+
+                default:
+                    throw new InvalidOperationException($"Unexpected value for action = {action}");
             }
         }
 
@@ -1004,6 +1007,9 @@ namespace Toastify.View
                     this.SongProgressBarLine.Background = new SolidColorBrush(ColorHelper.HexToColor(this.Settings.SongProgressBarForegroundColor));
                     this.SongProgressBarLineEllipse.Fill = new SolidColorBrush(ColorHelper.HexToColor(this.Settings.SongProgressBarForegroundColor));
                     break;
+
+                default:
+                    throw new InvalidOperationException($"Unexpected value for PropertyName = {e.PropertyName}");
             }
         }
 

Toastify/src/ViewModel/ToastViewModel.cs
@@ -136,6 +136,9 @@ namespace Toastify.ViewModel
                 case nameof(this.Settings.SongProgressBarForegroundColor):
                     this.NotifyPropertyChanged(e.PropertyName);
                     break;
+
+                default:
+                    break;
             }
         }
     }

Toastify/src/Helpers/WindowExtensions.cs
@@ -42,6 +42,9 @@ namespace Toastify.Helpers
                     case Key.LeftAlt:
                     case Key.RightAlt:
                         return 0x12;
+
+                    default:
+                        break;
                 }
             }
             return (uint)KeyInterop.VirtualKeyFromKey(key);

Toastify/src/ViewModel/SettingsViewModel.cs
@@ -170,6 +170,9 @@ namespace Toastify.ViewModel
                 case nameof(this.Settings.SongProgressBarForegroundColor):
                     this.NotifyPropertyChanged(nameof(this.SongProgressBarForegroundColor));
                     break;
+
+                default:
+                    break;
             }
         }
 
@@ -201,8 +204,14 @@ namespace Toastify.ViewModel
                         case 1:
                             this.Settings.SetDefaultToastColors();
                             break;
+
+                        default:
+                            throw new InvalidOperationException($"Unexpected value for CurrentToastTabIndex = {this.CurrentToastTabIndex}");
                     }
                     break;
+
+                default:
+                    throw new InvalidOperationException($"Unexpected value for CurrentTabIndex = {this.CurrentTabIndex}");
             }
         }
 

commit f599fdecf10d4a594b880a13d2a71f10d9574470

Fix error introduced in c86979f

Toastify/src/View/ToastView.xaml.cs
@@ -625,12 +625,6 @@ namespace Toastify.View
 
             switch (action)
             {
-                case ToastifyAction.PlayPause:
-                case ToastifyAction.NextTrack:
-                case ToastifyAction.PreviousTrack:
-                case ToastifyAction.ShowSpotify:
-                    break;
-
                 case ToastifyAction.SettingsSaved:
                     this.DisplayFlashContent("Settings Saved", string.Empty, DEFAULT_ICON, 1500.0);
                     break;
@@ -678,7 +672,8 @@ namespace Toastify.View
                     break;
 
                 default:
-                    throw new InvalidOperationException($"Unexpected value for action = {action}");
+                    // Ignore any other action
+                    break;
             }
         }
 
@@ -1009,7 +1004,8 @@ namespace Toastify.View
                     break;
 
                 default:
-                    throw new InvalidOperationException($"Unexpected value for PropertyName = {e.PropertyName}");
+                    // Ignore any other property
+                    break;
             }
         }
 

Toastify/src/ViewModel/ToastViewModel.cs
@@ -138,6 +138,7 @@ namespace Toastify.ViewModel
                     break;
 
                 default:
+                    // Ignore any other property
                     break;
             }
         }