bcb9389ca40c7c11943b63a73a5d18b0295b8aa9
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2018-03-05 18:41:58 +0100

    Add a couple of try-catch

--- Toastify/src/Helpers/SpotifyLocalAPIExtensions.cs
+++ Toastify/src/Helpers/SpotifyLocalAPIExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using SpotifyAPI.Local;
+using Toastify.Core;
 using Toastify.Model;
 
 namespace Toastify.Helpers
@@ -7,24 +8,45 @@ namespace Toastify.Helpers
     {
         public static void IncrementVolume(this SpotifyLocalAPI api)
         {
-            float currentVolume = api.GetSpotifyVolume();
-            float newVolume = currentVolume + Settings.Current.WindowsVolumeMixerIncrement;
-            api.SetSpotifyVolume(newVolume >= 100.0f ? 100.0f : newVolume);
+            try
+            {
+                float currentVolume = api.GetSpotifyVolume();
+                float newVolume = currentVolume + Settings.Current.WindowsVolumeMixerIncrement;
+                api.SetSpotifyVolume(newVolume >= 100.0f ? 100.0f : newVolume);
+            }
+            catch
+            {
+                Win32API.SendMediaKey(ToastifyAction.VolumeUp);
+            }
         }
 
         public static void DecrementVolume(this SpotifyLocalAPI api)
         {
-            float currentVolume = api.GetSpotifyVolume();
-            float newVolume = currentVolume - Settings.Current.WindowsVolumeMixerIncrement;
-            api.SetSpotifyVolume(newVolume <= 0.0f ? 0.0f : newVolume);
+            try
+            {
+                float currentVolume = api.GetSpotifyVolume();
+                float newVolume = currentVolume - Settings.Current.WindowsVolumeMixerIncrement;
+                api.SetSpotifyVolume(newVolume <= 0.0f ? 0.0f : newVolume);
+            }
+            catch
+            {
+                Win32API.SendMediaKey(ToastifyAction.VolumeDown);
+            }
         }
 
         public static void ToggleMute(this SpotifyLocalAPI api)
         {
-            if (api.IsSpotifyMuted())
-                api.UnMute();
-            else
-                api.Mute();
+            try
+            {
+                if (api.IsSpotifyMuted())
+                    api.UnMute();
+                else
+                    api.Mute();
+            }
+            catch
+            {
+                Win32API.SendMediaKey(ToastifyAction.Mute);
+            }
         }
     }
 }
