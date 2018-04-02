552703fd65764624319bc8c1d849d494829230f4
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2018-04-02 03:25:03 +0200

    v1.10.8

--- ToastifyAPI/Properties/AssemblyInfo.cs
+++ ToastifyAPI/Properties/AssemblyInfo.cs
@@ -31,11 +31,11 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.10.7.*")]
+[assembly: AssemblyVersion("1.10.8.*")]
 #pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
 #if DEBUG
-[assembly: AssemblyFileVersion("1.10.7 [DEBUG BUILD]")]
+[assembly: AssemblyFileVersion("1.10.8 [DEBUG BUILD]")]
 #elif TEST_RELEASE
-[assembly: AssemblyFileVersion("1.10.7 [TEST BUILD]")]
+[assembly: AssemblyFileVersion("1.10.8 [TEST BUILD]")]
 #endif
 #pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
