29ed1b86544e4e8786297c4e99869ef61f289762
radj307 <radj307@gmail.com>
2022-02-15 13:13:41 -0500

    fix settings group box name

--- VolumeControl/VolumeControlForm.Designer.cs
+++ VolumeControl/VolumeControlForm.Designer.cs
@@ -209,7 +209,7 @@
             this.groupBox1.Size = new System.Drawing.Size(134, 138);
             this.groupBox1.TabIndex = 20;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Settings";
             // 
             // Checkbox_AlwaysOnTop
             // 
