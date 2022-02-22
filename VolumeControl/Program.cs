a0e4d0f5faae49fb0d6435cf83cf55d7ac2f2f0d
radj307 <radj307@gmail.com>
2022-02-22 01:27:23 -0500

    only allow a single instance

--- VolumeControl/Program.cs
+++ VolumeControl/Program.cs
@@ -1,3 +1,7 @@
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
 namespace VolumeControl
 {
     internal static class Program
@@ -6,8 +10,19 @@ namespace VolumeControl
         [STAThread]
         private static void Main()
         {
+            Process thisProc = Process.GetCurrentProcess();
+            foreach (Process otherInst in Process.GetProcessesByName(thisProc.ProcessName))
+                if (thisProc.Id != otherInst.Id)
+                    throw new Exception("An instance of Volume Control is already running!");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
             ApplicationConfiguration.Initialize();
-            Application.Run(new VolumeControlForm());
+
+            var form = new VolumeControlForm();
+
+            Application.Run(form);
         }
     }
 }
