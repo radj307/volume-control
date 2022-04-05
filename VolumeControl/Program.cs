4f19493c5e7cb5983f887b87a5198168bb1a5086
radj307 <radj307@gmail.com>
2022-04-04 23:38:55 -0400

    add validation check for user.config

--- VolumeControl/Program.cs
+++ VolumeControl/Program.cs
@@ -1,4 +1,7 @@
+using System.Configuration;
 using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
 using VolumeControl.Core;
 using VolumeControl.Log;
 
@@ -12,15 +15,45 @@ namespace VolumeControl
         [STAThread]
         static void Main()
         {
-            FLog.Initialize();
             try
             {
-                ApplicationConfiguration.Initialize();
+                // This is the equivalent of calling ApplicationConfiguration.Initialize()
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+
+                try // validate settings
+                {
+                    // provoke an exception if the settings file is invalid
+                    Properties.Settings.Default.Reload();
+                    Properties.Settings.Default.Save();
+                }
+                catch (Exception ex) // settings are corrupted/invalid
+                {
+                    string? path = (ex.InnerException as ConfigurationErrorsException)?.Filename;
+                    if (MessageBox.Show(
+                        $"{Application.ProductName} failed to load the 'user.config' file located at:\n  '{path}'\n\nClick 'Yes' to reset all settings to their default values.\n(This will delete all of your custom settings!)\n\nClick 'No' if you want to attempt to repair it manually.",
+                        "Corrupted 'user.config' File!",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Error
+                    ) == DialogResult.Yes)
+                    {
+                        if (path != null)
+                        {
+                            File.Delete(path);
+                            Properties.Settings.Default.Reload();
+                            Properties.Settings.Default.Save();
+                        }
+                    }
+                    else Process.GetCurrentProcess().Kill(); //< kill self
+                }
+
+                FLog.Initialize();
 
                 VC_Static.Initialize(); // Initialize global statics
 
 #               if DEBUG
-                FLog.Log.WriteDebug("This binary was built in Debug configuration.");
+                FLog.Log.WriteDebug("Binary was built in 'DEBUG' configuration.");
                 if (!Properties.Settings.Default.AllowMultipleInstances)
                 {
                     var thisProc = Process.GetCurrentProcess();
@@ -34,7 +67,7 @@ namespace VolumeControl
                     }
                 }
 #               else
-                VC_Static.Log.WriteDebug("This binary was built in Release configuration.");
+                FLog.Log.WriteDebug("Binary was built in 'RELEASE' configuration.");
 
                 if (!Properties.Settings.Default.AllowMultipleInstances)
                 {
