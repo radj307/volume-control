0265af0f2766706c570f3a1860453dca492ebdc8
radj307 <radj307@gmail.com>
2022-04-27 12:01:07 -0400

    Add a numeric up down with label control wrapper

---
+++ VolumeControl.Core.Controls/NumericUpDownWithLabel.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace VolumeControl.Core.Controls
+{
+    partial class NumericUpDownWithLabel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flp = new System.Windows.Forms.FlowLayoutPanel();
+            this.label = new System.Windows.Forms.Label();
+            this.nud = new System.Windows.Forms.NumericUpDown();
+            this.flp.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flp
+            // 
+            this.flp.BackColor = System.Drawing.Color.Transparent;
+            this.flp.Controls.Add(this.label);
+            this.flp.Controls.Add(this.nud);
+            this.flp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flp.ForeColor = System.Drawing.Color.Transparent;
+            this.flp.Location = new System.Drawing.Point(0, 0);
+            this.flp.Margin = new System.Windows.Forms.Padding(0);
+            this.flp.Name = "flp";
+            this.flp.Size = new System.Drawing.Size(122, 26);
+            this.flp.TabIndex = 0;
+            this.flp.WrapContents = false;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label.Location = new System.Drawing.Point(0, 0);
+            this.label.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.label.Name = "label";
+            this.label.Padding = new System.Windows.Forms.Padding(0, 2, 0, 2);
+            this.label.Size = new System.Drawing.Size(59, 19);
+            this.label.TabIndex = 1;
+            this.label.Text = "Label Text";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label.MarginChanged += new System.EventHandler(this.SizeToFit);
+            this.label.TextChanged += new System.EventHandler(this.SizeToFit);
+            // 
+            // nud
+            // 
+            this.nud.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.nud.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nud.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nud.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.nud.Location = new System.Drawing.Point(63, 0);
+            this.nud.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.nud.Name = "nud";
+            this.nud.Size = new System.Drawing.Size(55, 23);
+            this.nud.TabIndex = 0;
+            this.nud.MarginChanged += new System.EventHandler(this.SizeToFit);
+            this.nud.Resize += new System.EventHandler(this.SizeToFit);
+            // 
+            // NumericUpDownWithLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.flp);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Name = "NumericUpDownWithLabel";
+            this.Size = new System.Drawing.Size(122, 26);
+            this.Load += new System.EventHandler(this.NumericUpDownWithLabel_Load);
+            this.flp.ResumeLayout(false);
+            this.flp.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flp;
+        private Label label;
+        private NumericUpDown nud;
+    }
+}
---
+++ VolumeControl.Core.Controls/NumericUpDownWithLabel.cs
@@ -0,0 +1,136 @@
+﻿using System.ComponentModel;
+
+namespace VolumeControl.Core.Controls
+{
+    [DefaultEvent("ValueChanged")]
+    public partial class NumericUpDownWithLabel : UserControl
+    {
+        public NumericUpDownWithLabel()
+        {
+            InitializeComponent();
+        }
+
+        private bool _allowAutoSize = false;
+
+        #region Events
+        /// <summary>
+        /// Occurs when the System.Windows.Forms.NumericUpDown.Value property has been changed in some way.
+        /// </summary>
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)] // ensure this is shown in the designer
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Bindable(true)]
+        [Browsable(true)]
+        public event EventHandler? ValueChanged
+        {
+            add => nud.ValueChanged += value;
+            remove => nud.ValueChanged -= value;
+        }
+        #endregion Events
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the value assigned to the spin box (also known as an up-down control).
+        /// </summary>
+        /// <returns>The numeric value of the System.Windows.Forms.NumericUpDown control.</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">The assigned value is less than the System.Windows.Forms.NumericUpDown.Minimum property value. -or- The assigned value is greater than the System.Windows.Forms.NumericUpDown.Maximum property value.</exception>
+        public decimal Value
+        {
+            get => nud.Value;
+            set => nud.Value = value;
+        }
+        /// <summary>
+        /// Gets or sets the minimum allowed value for the spin box (also known as an up-down control).
+        /// </summary>
+        /// <returns>The minimum allowed value for the spin box. The default value is 0.</returns>
+        public decimal Minimum
+        {
+            get => nud.Minimum;
+            set => nud.Minimum = value;
+        }
+        /// <summary>
+        /// Gets or sets the maximum value for the spin box (also known as an up-down control).
+        /// </summary>
+        /// <returns>The maximum value for the spin box. The default value is 100.</returns>
+        public decimal Maximum
+        {
+            get => nud.Maximum;
+            set => nud.Maximum = value;
+        }
+        /// <summary>
+        /// Gets or sets the text associated with this control.<br/>
+        /// This changes the label text.
+        /// </summary>
+        /// <returns>The text associated with this control.</returns>
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)] // ensure this is shown in the designer
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Bindable(true)]
+        [Browsable(true)]
+        public string LabelText
+        {
+            get => label.Text;
+            set => label.Text = value;
+        }
+        /// <summary>
+        /// Gets or sets the width of the numeric up down control.
+        /// </summary>
+        public int BoxWidth
+        {
+            get => nud.Size.Width;
+            set => nud.Size = new(value, nud.Size.Height);
+        }
+        public override Color ForeColor
+        {
+            get => label.ForeColor;
+            set => label.ForeColor = nud.ForeColor = value;
+        }
+        public override Color BackColor
+        {
+            get => nud.BackColor;
+            set => nud.BackColor = value;
+        }
+
+        /// <summary>
+        /// Gets the flow layout panel subcontrol.
+        /// </summary>
+        public FlowLayoutPanel FlowLayoutPanel => flp;
+        /// <summary>
+        /// Gets the numeric up down subcontrol.
+        /// </summary>
+        public NumericUpDown NumericUpDown => nud;
+        /// <summary>
+        /// Gets the label subcontrol.
+        /// </summary>
+        public Label Label => label;
+        #endregion Properties
+
+        #region Methods
+        /// <summary>
+        /// Sets <see cref="Control.Size"/> to fit the subcontrol contents.
+        /// </summary>
+        public void SizeToFit()
+        {
+            if (!_allowAutoSize)
+                return;
+
+            SuspendLayout();
+
+            int height = nud.Location.Y + nud.Size.Height + nud.Margin.Bottom;
+            int width = nud.Location.X + BoxWidth + nud.Margin.Right;
+
+            Size = new Size(width, height);
+
+            UpdateBounds();
+            ResumeLayout();
+        }
+        private void SizeToFit(object sender, EventArgs e) => SizeToFit();
+        #endregion Methods
+
+        private void NumericUpDownWithLabel_Load(object sender, EventArgs e)
+        {
+            if (!this.DesignMode)
+            {
+                _allowAutoSize = true;
+            }
+        }
+    }
+}
