5673f87dc404e934456969b3a08a9e0049c8f3da
radj307 <radj307@gmail.com>
2022-05-22 10:17:01 -0400

    fix xcopy errors. Add .pdb and .xml doc files to sdk

--- VolumeControl.API/VCAPI.cs
+++ VolumeControl.API/VCAPI.cs
@@ -7,11 +7,30 @@ namespace VolumeControl.API
     /// <summary>Struct containing various useful object references.<br/>Cannot be directly instantiated, use the <see cref="Default"/> static property.</summary>
     public struct VCAPI
     {
+        #region Statics
+        /// <summary>
+        /// This is the default instance of VCAPI.<br/>You must use this to access the API.
+        /// </summary>
         public static VCAPI Default { get; internal set; }
+        /// <summary>
+        /// This is the default log instance.
+        /// </summary>
         public static LogWriter Log => FLog.Log;
+        #endregion Statics
 
+        #region Properties
+        /// <summary>
+        /// This is the global <see cref="AudioAPI"/>.
+        /// </summary>
         public AudioAPI AudioAPI { get; internal set; }
+        /// <summary>
+        /// This is the global <see cref="HotkeyManager"/>.
+        /// </summary>
         public HotkeyManager HotkeyManager { get; internal set; }
+        /// <summary>
+        /// This is the handle of the main window, for use with Windows API methods in <see cref="User32"/> or elsewhere.
+        /// </summary>
         public IntPtr MainWindowHWnd { get; internal set; }
+        #endregion Properties
     }
 }
