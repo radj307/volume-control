d4daf9d2666b2894f0eb594c931e0308bd2ba5c4
radj307 <radj307@gmail.com>
2023-08-13 16:41:38 -0400

    Made volume sliders controllable using the mouse wheel
    The slider will change by whatever the Volume Step Size is set to.

---
+++ VolumeControl.WPF/MouseWheelSliderBehavior.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace VolumeControl.WPF
+{
+    /// <summary>
+    /// Behavior that allows the mouse wheel to change the value of <see cref="Slider"/> controls.
+    /// </summary>
+    public class MouseWheelSliderBehavior : Behavior<Slider>
+    {
+        public double Amount
+        {
+            get => (double)GetValue(AmountProperty);
+            set => SetValue(AmountProperty, value);
+        }
+
+        public static readonly DependencyProperty AmountProperty
+            = DependencyProperty.RegisterAttached(
+                nameof(Amount), 
+                typeof(double), 
+                typeof(MouseWheelSliderBehavior), 
+                new UIPropertyMetadata(0.0));
+
+        private void AssociatedObject_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (Amount == 0.0) return;
+            Slider slider = (Slider)sender;
+            if (e.Delta > 0)
+            {
+                slider.Value += Amount;
+            }
+            else
+            {
+                slider.Value -= Amount;
+            }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.PreviewMouseWheel += this.AssociatedObject_PreviewMouseWheel;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.PreviewMouseWheel -= this.AssociatedObject_PreviewMouseWheel;
+        }
+    }
+}
