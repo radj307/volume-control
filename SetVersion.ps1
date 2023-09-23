af18d98a2894bd0a12a46c68de47fc538e1e08c9
radj307 <radj307@gmail.com>
2023-09-23 18:15:30 -0400

    fix SetVersion.ps1 error

--- SetVersion.ps1
+++ SetVersion.ps1
@@ -1,26 +1,29 @@
 # ARGUMENT: FILEPATH
 param([String]$Path="")
 
+# Resolve Path
 $Path = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($(Get-Location), $Path))
 
-"Setting Version Attributes in '$Path'"
-
-# Get Git Tag
+# Get Git Tag, 3-part tag, & copyright notice
 $TAG = "$(git describe --tags --abbrev=0)"
 $TAG -cmatch '(?<MAJOR>\d+?)\.(?<MINOR>\d+?)\.(?<PATCH>\d+?)(?<EXTRA>.*)' > $null
 $TAG_3_PART = $Matches.MAJOR + '.' + $Matches.MINOR + '.' + $Matches.PATCH
+$COPYRIGHT = "Copyright © $((Get-Date).Year) by `$`(Authors`)"
 
-"Tag:         '$TAG'"
-if ($TAG -ne $TAG_3_PART) {
-    "3-Part Tag:  '$TAG_3_PART'"
-}
+"Reading '$Path'"
 
-# Read file
+# Read XML file
 [xml]$CONTENT = Get-Content -Path "$Path"
 
-$CONTENT.Project.PropertyGroup.FileVersion  = $TAG_3_PART
-$CONTENT.Project.PropertyGroup.Version      = $TAG
-$CONTENT.Project.PropertyGroup.Copyright    = "Copyright © $((Get-Date).Year) by `$`(Authors`)"
+# doing this prevents failure when there are multiple PropertyGroup nodes:
+$TARGET_NODE = $CONTENT.SelectSingleNode("//Project/PropertyGroup")
+
+$TARGET_NODE.FileVersion  = $TAG_3_PART
+"Set FileVersion to '$TAG_3_PART' (was '$($TARGET_NODE.FileVersion)')"
+$TARGET_NODE.Version      = $TAG
+"Set Version to '$TAG' (was '$($TARGET_NODE.Version)')"
+$TARGET_NODE.Copyright    = $COPYRIGHT
+"Set Copyright to '$COPYRIGHT' (was '$($TARGET_NODE.Copyright)')"
 
 $CONTENT.Save("$Path")
 
