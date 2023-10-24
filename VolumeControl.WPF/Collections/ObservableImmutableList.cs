1b580dcc726054ab1cbf96d22cf4fdd631e35027
radj307 <radj307@gmail.com>
2023-10-23 22:04:08 -0400

    merge files
    merge ObservableImmutableList base types into the same file & suppress warnings

--- VolumeControl.WPF/Collections/ObservableImmutableList.cs
+++ VolumeControl.WPF/Collections/ObservableImmutableList.cs
@@ -1,14 +1,236 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace VolumeControl.WPF.Collections
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    [SuppressPropertyChangedWarnings]
+    public abstract class ObservableCollectionObject : INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        #region Private
+
+        private bool _lockObjWasTaken;
+        private readonly object _lockObj;
+        private int _lock; // 0=unlocked		1=locked
+
+        #endregion Private
+
+        #region Public Properties
+
+        public LockTypeEnum LockType { get; }
+
+        #endregion Public Properties
+
+        #region Constructor
+
+        protected ObservableCollectionObject(LockTypeEnum lockType)
+        {
+            this.LockType = lockType;
+            _lockObj = new object();
+        }
+
+        #endregion Constructor
+
+        #region SpinWait/PumpWait Methods
+
+        // note : find time to put all these methods into a helper class instead of in a base class
+
+        // returns a valid dispatcher if this is a UI thread (can be more than one UI thread so different dispatchers are possible); null if not a UI thread
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected Dispatcher GetDispatcher() => Dispatcher.FromThread(Thread.CurrentThread);
+
+        protected void WaitForCondition(Func<bool> condition)
+        {
+            Dispatcher? dispatcher = this.GetDispatcher();
+
+            if (dispatcher == null)
+            {
+                switch (this.LockType)
+                {
+                case LockTypeEnum.SpinWait:
+                    SpinWait.SpinUntil(condition); // spin baby... 
+                    break;
+                case LockTypeEnum.Lock:
+                    bool isLockTaken = false;
+                    Monitor.Enter(_lockObj, ref isLockTaken);
+                    _lockObjWasTaken = isLockTaken;
+                    break;
+                }
+                return;
+            }
+
+            _lockObjWasTaken = true;
+            this.PumpWait_PumpUntil(dispatcher, condition);
+        }
+
+        protected void PumpWait_PumpUntil(Dispatcher dispatcher, Func<bool> condition)
+        {
+            var frame = new DispatcherFrame();
+            BeginInvokePump(dispatcher, frame, condition);
+            Dispatcher.PushFrame(frame);
+        }
+
+        private static void BeginInvokePump(Dispatcher dispatcher, DispatcherFrame frame, Func<bool> condition) => dispatcher.BeginInvoke
+                (
+                DispatcherPriority.DataBind,
+
+
+                    () =>
+                    {
+                        frame.Continue = !condition();
+
+                        if (frame.Continue)
+                            BeginInvokePump(dispatcher, frame, condition);
+                    }
+
+                );
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void DoEvents()
+        {
+            Dispatcher? dispatcher = this.GetDispatcher();
+            if (dispatcher == null)
+                return;
+
+            var frame = new DispatcherFrame();
+            _ = dispatcher.BeginInvoke(DispatcherPriority.DataBind, new DispatcherOperationCallback(ExitFrame), frame);
+            Dispatcher.PushFrame(frame);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static object ExitFrame(object frame)
+        {
+            ((DispatcherFrame)frame).Continue = false;
+            return null!;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected bool TryLock() => this.LockType switch
+        {
+            LockTypeEnum.SpinWait => Interlocked.CompareExchange(ref _lock, 1, 0) == 0,
+            LockTypeEnum.Lock => Monitor.TryEnter(_lockObj),
+            _ => false,
+        };
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected void Lock()
+        {
+            switch (this.LockType)
+            {
+            case LockTypeEnum.SpinWait:
+                this.WaitForCondition(() => Interlocked.CompareExchange(ref _lock, 1, 0) == 0);
+                break;
+            case LockTypeEnum.Lock:
+                this.WaitForCondition(() => Monitor.TryEnter(_lockObj));
+                break;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected void Unlock()
+        {
+            switch (this.LockType)
+            {
+            case LockTypeEnum.SpinWait:
+                _lock = 0;
+                break;
+            case LockTypeEnum.Lock:
+                if (_lockObjWasTaken)
+                    Monitor.Exit(_lockObj);
+                _lockObjWasTaken = false;
+                break;
+            }
+        }
+
+        #endregion SpinWait/PumpWait Methods
+
+        #region INotifyCollectionChanged
+
+        public virtual event NotifyCollectionChangedEventHandler? CollectionChanged;
+        protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs args)
+        {
+            NotifyCollectionChangedEventHandler? notifyCollectionChangedEventHandler = CollectionChanged;
+
+            if (notifyCollectionChangedEventHandler == null)
+                return;
+
+            foreach (NotifyCollectionChangedEventHandler handler in notifyCollectionChangedEventHandler.GetInvocationList())
+            {
+                if (handler.Target is DispatcherObject dispatcherObject && !dispatcherObject.CheckAccess())
+                    _ = dispatcherObject.Dispatcher.Invoke(DispatcherPriority.DataBind, handler, this, args);
+                else
+                {
+                    handler(this, args);
+                }
+            }
+        }
+
+        protected virtual void RaiseNotifyCollectionChanged() => this.RaiseNotifyCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+
+        protected virtual void RaiseNotifyCollectionChanged(NotifyCollectionChangedEventArgs args)
+        {
+            this.RaisePropertyChanged("Count");
+            this.RaisePropertyChanged("Item[]");
+            this.OnCollectionChanged(args);
+        }
+
+        #endregion INotifyCollectionChanged
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion INotifyPropertyChanged
+
+        #region Nested Types
+
+        public enum LockTypeEnum
+        {
+            SpinWait,
+            Lock
+        }
+
+        #endregion Nested Types
+    }
+    /// <inheritdoc cref="ObservableCollection{T}"/>
+    public class ObservableList<T> : ObservableCollection<T>
+    {
+        /// <inheritdoc/>
+        protected override void InsertItem(int index, T item)
+        {
+            this.CheckReentrancy();
+            base.InsertItem(index, item);
+            base.OnPropertyChanged(new("Count"));
+            base.OnPropertyChanged(new("Items[]"));
+        }
+        /// <inheritdoc cref="List{T}.AddRange(IEnumerable{T})"/>
+        public void AddRange(IEnumerable<T> range)
+        {
+            foreach (T item in range) this.Add(item);
+        }
+        /// <inheritdoc cref="List{T}.RemoveAll(Predicate{T})"/>
+        public void RemoveAll(Predicate<T> predicate)
+        {
+            for (int i = this.Count - 1; i >= 0; --i)
+            {
+                if (predicate(this[i]))
+                    this.RemoveAt(i);
+            }
+        }
+    }
+    [SuppressPropertyChangedWarnings]
     public class ObservableImmutableList<T> : ObservableCollectionObject, IList, ICollection, IEnumerable, IList<T>, IImmutableList<T>, ICollection<T>, IEnumerable<T>, IReadOnlyList<T>, IReadOnlyCollection<T>, INotifyCollectionChanged, INotifyPropertyChanged
     {
         #region Private
@@ -588,4 +810,5 @@ namespace VolumeControl.WPF.Collections
             }
         }
     }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
