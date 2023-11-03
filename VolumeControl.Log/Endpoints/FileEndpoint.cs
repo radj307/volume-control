7502cb6c21d8427dc058785951920ace194cab8a
radj307 <radj307@gmail.com>
2023-11-03 12:26:30 -0400

    use FileInfo to open stream in FileEndpoint

--- VolumeControl.Log/Endpoints/FileEndpoint.cs
+++ VolumeControl.Log/Endpoints/FileEndpoint.cs
@@ -1,4 +1,5 @@
-﻿using VolumeControl.Log.Interfaces;
+﻿using System.Text;
+using VolumeControl.Log.Interfaces;
 
 namespace VolumeControl.Log.Endpoints
 {
@@ -13,21 +14,43 @@ namespace VolumeControl.Log.Endpoints
         /// <param name="enabled">Whether the endpoint is already enabled when constructed or not.</param>
         public FileEndpoint(string path, bool enabled) : base(enabled)
         {
+            _fileInfo = new(path);
             _path = path;
         }
         #endregion Constructors
 
         #region Properties
         /// <summary>
-        /// The location of the log file.
+        /// Gets or sets the file info object for the log file.
+        /// </summary>
+        public FileInfo FileInfo
+        {
+            get => _fileInfo;
+            set
+            {
+                if (value == _fileInfo) return;
+
+                _fileInfo = value;
+                _path = _fileInfo.FullName;
+                NotifyPropertyChanged();
+                NotifyPropertyChanged(nameof(Path));
+            }
+        }
+        private FileInfo _fileInfo;
+        /// <summary>
+        /// Gets or sets the location of the log file.
         /// </summary>
         public string Path
         {
             get => _path;
             set
             {
+                if (value.Equals(_path, StringComparison.Ordinal)) return;
+
                 _path = value;
+                _fileInfo = new(_path);
                 NotifyPropertyChanged();
+                NotifyPropertyChanged(nameof(FileInfo));
             }
         }
         private string _path;
@@ -39,14 +62,13 @@ namespace VolumeControl.Log.Endpoints
         #endregion Properties
 
         #region Methods
-        internal StreamWriter? GetWriter(FileMode fileMode, FileAccess fileAccess, FileShare fileShare) => !this.IsWritingEnabled || this.Path.Length == 0 ? null : new StreamWriter(File.Open(this.Path, fileMode, fileAccess, fileShare));
         /// <summary>
         /// Gets a new <see cref="StreamWriter"/> instance for the file. The caller is responsible for disposing of it.
         /// </summary>
         /// <returns>A new <see cref="StreamWriter"/> instance when this endpoint is enabled; otherwise <see langword="null"/>.</returns>
-        public StreamWriter GetStreamWriter() => !this.IsWritingEnabled || this.Path.Length == 0
+        public virtual StreamWriter GetStreamWriter() => !this.IsWritingEnabled || this.Path.Length == 0
                 ? null!
-                : this.GetWriter(FileMode.Append, FileAccess.Write, FileShare.ReadWrite)!;
+                : new(FileInfo.Open(FileMode.Append, FileAccess.Write, FileShare.Read), Encoding.UTF8)!;
         /// <inheritdoc/>
         public override TextWriter GetTextWriter() => GetStreamWriter();
         /// <inheritdoc/>
