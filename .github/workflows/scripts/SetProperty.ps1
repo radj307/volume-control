ddc6e0b9b60a447dfc98d68b60abc307f0712c49
radj307 <radj307@gmail.com>
2023-12-13 18:27:07 -0500

    update SetProperty.ps1

--- .github/workflows/scripts/SetProperty.ps1
+++ .github/workflows/scripts/SetProperty.ps1
@@ -1,19 +1,14 @@
-# SetVersion.ps1
-#   Powershell helper script for CI scripts to update .csproj files.
-#
-# Usage:
-#   SetVersion -Path <CSPROJ_PATH> [<PROPERTY>=<VALUE>]...
-#
-# Example:
-#   SetVersion -Path MyProject/MyProject.csproj  Version=$(git describe --tags --abbrev=0)
+# SetProperty.ps1 by radj307
+#   Powershell helper script for CI scripts to get or set properties in a .csproj or .props file.
 param(
-    [Parameter(Mandatory=$true,HelpMessage="Path to the target .csproj file.")][String]$Path="",
-    [Parameter(ValueFromRemainingArguments=$true)][String[]]$Properties,
-    [Parameter(Mandatory=$false,HelpMessage="Whether to write output or not.")][switch]$Quiet
+    [Parameter(Mandatory = $true, HelpMessage = "Path to the target file.")][String]$Path = "",
+    [Parameter(ValueFromRemainingArguments = $true)][String[]]$Properties,
+    [Parameter(Mandatory = $false, HelpMessage = "Prevents log messages from being written to the console. Useful with property getters.")][switch]$Quiet,
+    [Parameter(Mandatory = $false, HelpMessage = "Removes properties when setting them to an empty value.")][switch]$CanRemove,
+    [Parameter(Mandatory = $false, HelpMessage = "Trims single & double quotes from values.")][switch]$TrimQuotes
 )
 
-function Add-XMLAttribute([System.Xml.XmlNode] $Node, $Name, $Value)
-{
+function Add-XMLAttribute([System.Xml.XmlNode] $Node, $Name, $Value) {
     $attrib = $Node.OwnerDocument.CreateAttribute($Name)
     $attrib.Value = $Value
     $Node.Attributes.Append($attrib)
@@ -34,43 +29,57 @@ $TARGET_NODE = $CONTENT.SelectSingleNode("//Project/PropertyGroup")
 
 $hasFileChanged = $false
 
-foreach ($prop in $Properties)
-{
-    $v = $prop -split "=",2
-    if ($null -ne $v[1])
-    { # Property Setter
-        $v = $prop -split "=",2
+foreach ($prop in $Properties) {
+    $v = $prop -split "=", 2
+
+    if ($null -ne $v[1]) {
+        # Property Setter
+        $v = $prop -split "=", 2
+
+        if ($TrimQuotes) {
+            $v[1] = $v[1].Trim("`'", "`"")
+        }
 
         $nodeName = "$($v[0])"
+        $valueIsEmpty = $($v[1].Length -eq 0)
 
         $previousValue = $TARGET_NODE.$nodeName
-        if ($v[1] -eq $previousValue)
-        {
+        if (-not $valueIsEmpty -and $v[1] -eq $previousValue) {
             if (-not $Quiet) {
                 "$($v[0]) was already set to $previousValue"
             }
             continue
         }
 
-        if ($v[1].Length -eq 0)
-        { # Delete the element if the incoming value is blank
-            $nodeToRemove = $TARGET_NODE.SelectSingleNode("./$nodeName")
-            if ($null -ne $nodeToRemove) {
-                $TARGET_NODE.RemoveChild($nodeToRemove) > $null
+        if ($valueIsEmpty) {
+            if ($CanRemove) {
+                # Delete the element if the incoming value is blank
+                $nodeToRemove = $TARGET_NODE.SelectSingleNode("./$nodeName")
+                if ($null -ne $nodeToRemove) {
+                    $TARGET_NODE.RemoveChild($nodeToRemove) > $null
 
-                if (-not $Quiet) { "Removed $nodeName" }
+                    if (-not $Quiet) { "Removed $nodeName" }
+                }
+                elseif (-not $Quiet) {
+                    if ($CanRemove) {
+                        "Not creating $nodeName. (Value is blank & '-CanRemove' was specified)"
+                    }
+                    else {
+                        "Node $nodeName doesn't exist."
+                    }
+                }
             }
-            elseif (-not $Quiet) { "Node $nodeName doesn't exist." }
+            elseif (-not $Quiet) { "Not removing $nodeName. (Specify the '-CanRemove' switch to enable removing properties." }
         }
-        elseif ($null -eq $previousValue)
-        { # Create the element if it doesn't exist
+        elseif ($null -eq $previousValue) {
+            # Create the element if it doesn't exist
             $newNode = $CONTENT.CreateElement($nodeName)
             $TARGET_NODE.AppendChild($newNode).InnerText = $v[1]
 
             if (-not $Quiet) { "Created $nodeName with value `"$($v[1])`"" }
         }
-        else
-        { # Set the existing element
+        else {
+            # Set the existing element
             $TARGET_NODE.$nodeName = $v[1]
             
             if (-not $Quiet) { "Set $nodeName to `"$($v[1])`" (Was `"$previousValue`")" }
@@ -79,11 +88,12 @@ foreach ($prop in $Properties)
         $hasFileChanged = $true
     }
     else {
-        Write-Output $TARGET_NODE.$v
+        # Property Getter
+        Write-Host $TARGET_NODE.$v
     }
 }
 
-if ($hasFileChanged){
+if ($hasFileChanged) {
     $CONTENT.Save("$Path")
 
     if (-not $Quiet) {
