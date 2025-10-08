200e71ac8960bfebd3cc9c04962e8afe14f64ce5
radj307 <radj307@gmail.com>
2025-10-08 14:56:57 -0400

    implement Active Application SetVolume

--- VolumeControl.HotkeyActions/ActiveApplicationActions.cs
+++ VolumeControl.HotkeyActions/ActiveApplicationActions.cs
@@ -5,6 +5,8 @@ using VolumeControl.CoreAudio;
 using VolumeControl.SDK;
 using VolumeControl.SDK.DataTemplates;
 using CoreAudio;
+using System.Windows.Controls.Primitives;
+using VolumeControl.Core;
 
 namespace VolumeControl.HotkeyActions
 {
@@ -15,12 +17,20 @@ namespace VolumeControl.HotkeyActions
     public sealed class ActiveApplicationActions
     {
         #region Fields
+        // NOTE: Don't change any names without also changing the translation keys in en-HotkeyActions.json!
+
         // Select target
         private const string Setting_SelectTarget_Name = "Select Session";
         private const string Setting_SelectTarget_Description = "Selects the session when the action is triggered.";
         // Volume Step
         private const string Setting_VolumeStep_Name = "Volume Step Override";
         private const string Setting_VolumeStep_Description = "Overrides the default volume step for this action.";
+        // SetVolume - Volume Level
+        private const string Setting_VolumeLevel_Name = "Volume Level";
+        private const string Setting_VolumeLevel_Description = "The volume level to set the target session(s) to.";
+        // SetVolume - Mute State
+        private const string Setting_MuteState_Name = "Mute State";
+        private const string Setting_MuteState_Description = "The mute state to set the target session(s) to.";
         #endregion Fields
 
         #region Properties
@@ -79,19 +89,23 @@ namespace VolumeControl.HotkeyActions
         #endregion Functions
 
         #region Methods
+
+        #region VolumeUp
         [HotkeyAction(Description = "Increases the volume of the current foreground application.")]
         [HotkeyActionSetting(Setting_SelectTarget_Name, typeof(bool), Description = Setting_SelectTarget_Description)]
         [HotkeyActionSetting(Setting_VolumeStep_Name, typeof(int), "VolumeStepDataTemplate", DefaultValue = 2, Description = Setting_VolumeStep_Description, IsToggleable = true)]
         public void VolumeUp(object? sender, HotkeyPressedEventArgs e)
         {
-            var volumeStep = e.GetValueOrDefault(Setting_VolumeStep_Name, VCAPI.Settings.VolumeStepSize);
             var sessions = GetActiveSessions();
-            foreach (var session in sessions)
-            {
-                session.Volume += volumeStep;
-            }
             if (sessions.Count > 0)
             {
+                var volumeStep = e.GetValueOrDefault(Setting_VolumeStep_Name, VCAPI.Settings.VolumeStepSize);
+
+                foreach (var session in sessions)
+                {
+                    session.Volume += volumeStep;
+                }
+
                 if (e.GetValue<bool>(Setting_SelectTarget_Name))
                 {
                     VCAPI.AudioSessionMultiSelector.SetSelectedSessionsOrCurrentSession(sessions);
@@ -99,26 +113,74 @@ namespace VolumeControl.HotkeyActions
                 VCAPI.ShowSessionListNotification(sessions);
             }
         }
+        #endregion VolumeUp
+
+        #region VolumeDown
         [HotkeyAction(Description = "Decreases the volume of the current foreground application.")]
         [HotkeyActionSetting(Setting_SelectTarget_Name, typeof(bool), Description = Setting_SelectTarget_Description)]
         [HotkeyActionSetting(Setting_VolumeStep_Name, typeof(int), "VolumeStepDataTemplate", DefaultValue = 2, Description = Setting_VolumeStep_Description, IsToggleable = true)]
         public void VolumeDown(object? sender, HotkeyPressedEventArgs e)
         {
-            var volumeStep = e.GetValueOrDefault(Setting_VolumeStep_Name, VCAPI.Settings.VolumeStepSize);
             var sessions = GetActiveSessions();
-            foreach (var session in sessions)
-            {
-                session.Volume -= volumeStep;
-            }
             if (sessions.Count > 0)
             {
+                var volumeStep = e.GetValueOrDefault(Setting_VolumeStep_Name, VCAPI.Settings.VolumeStepSize);
+
+                foreach (var session in sessions)
+                {
+                    session.Volume -= volumeStep;
+                }
+
                 if (e.GetValue<bool>(Setting_SelectTarget_Name))
                 {
                     VCAPI.AudioSessionMultiSelector.SetSelectedSessionsOrCurrentSession(sessions);
                 }
+
                 VCAPI.ShowSessionListNotification(sessions);
             }
         }
+        #endregion VolumeDown
+
+        #region SetVolume
+        [HotkeyAction(Description = "Sets the volume and/or mute state of the session(s).")]
+        [HotkeyActionSetting(Setting_SelectTarget_Name, typeof(bool), Description = Setting_SelectTarget_Description)]
+        [HotkeyActionSetting(Setting_VolumeLevel_Name, typeof(int), "VolumeLevelDataTemplate", DefaultValue = 50, Description = Setting_VolumeLevel_Description, IsToggleable = true, StartsEnabled = true)]
+        [HotkeyActionSetting(Setting_MuteState_Name, typeof(bool), Description = Setting_MuteState_Description, IsToggleable = true)]
+        public void SetVolume(object? sender, HotkeyPressedEventArgs e)
+        {
+            var sessions = GetActiveSessions();
+
+            if (sessions.Count > 0)
+            {
+                var (setVolumeLevel, volumeLevel) = e.GetSetting<int>(Setting_VolumeLevel_Name);
+                var (setMuteState, muteState) = e.GetSetting<bool>(Setting_MuteState_Name);
+
+                if (setVolumeLevel || setMuteState)
+                {
+                    foreach (var session in sessions)
+                    {
+                        if (setVolumeLevel)
+                        {
+                            session.Volume = volumeLevel;
+                        }
+                        if (setMuteState)
+                        {
+                            session.Mute = muteState;
+                        }
+                    }
+                }
+
+                if (e.GetValue<bool>(Setting_SelectTarget_Name))
+                {
+                    VCAPI.AudioSessionMultiSelector.SetSelectedSessionsOrCurrentSession(sessions);
+                }
+
+                VCAPI.ShowSessionListNotification(sessions);
+            }
+        }
+        #endregion SetVolume
+
+        #region Mute
         [HotkeyAction(Description = "Mutes the current foreground application.")]
         [HotkeyActionSetting(Setting_SelectTarget_Name, typeof(bool), Description = Setting_SelectTarget_Description)]
         public void Mute(object? sender, HotkeyPressedEventArgs e)
@@ -137,6 +199,9 @@ namespace VolumeControl.HotkeyActions
                 VCAPI.ShowSessionListNotification(sessions);
             }
         }
+        #endregion Mute
+
+        #region Unmute
         [HotkeyAction(Description = "Unmutes the current foreground application.")]
         [HotkeyActionSetting(Setting_SelectTarget_Name, typeof(bool), Description = Setting_SelectTarget_Description)]
         public void Unmute(object? sender, HotkeyPressedEventArgs e)
@@ -155,6 +220,9 @@ namespace VolumeControl.HotkeyActions
                 VCAPI.ShowSessionListNotification(sessions);
             }
         }
+        #endregion Unmute
+
+        #region ToggleMute
         [HotkeyAction(Description = "(Un)Mutes the current foreground application.")]
         [HotkeyActionSetting(Setting_SelectTarget_Name, typeof(bool), Description = Setting_SelectTarget_Description)]
         public void ToggleMute(object? sender, HotkeyPressedEventArgs e)
@@ -173,6 +241,9 @@ namespace VolumeControl.HotkeyActions
                 VCAPI.ShowSessionListNotification(sessions);
             }
         }
+        #endregion ToggleMute
+
+        #region Show
         [HotkeyAction(Description = "Shows the active application notification if there are sessions to show.")]
         public void Show(object? sender, HotkeyPressedEventArgs e)
         {
@@ -180,6 +251,8 @@ namespace VolumeControl.HotkeyActions
             if (sessions.Count > 0)
                 VCAPI.ShowSessionListNotification(sessions);
         }
+        #endregion Show
+
         #endregion Methods
     }
 }
